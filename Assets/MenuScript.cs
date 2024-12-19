6301d696ab9a949e382d7794e50713efb665c8c2
alexman37 <alexackler@gmail.com>
2024-12-18 22:06:54 -0500

    The builds on ios dont look horrible but we need to figure out a better testing process

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -11,10 +11,21 @@ public class MenuScript : MonoBehaviour
     public int numLevels = 2;
     public WordGen.WordDB database = WordGen.WordDB.CROSSWORD;
 
+    public GameObject titleCard;
+    public GameObject playButtons;
+
+    private Rect safeArea;
+
     // Start is called before the first frame update
     void Start()
     {
         newGame += filler;
+
+        // Configure UI to the phone's settings
+        safeArea = Screen.safeArea;
+
+        //setLocAndScale(titleCard, new Vector2(0.15f, 0.15f));
+        //setLocAndScale(playButtons, new Vector2(0.15f, 0.5f));
     }
 
     // Update is called once per frame
@@ -28,6 +39,28 @@ public class MenuScript : MonoBehaviour
         Debug.Log("Setting up new game");
         GameManagerSc.setParametersOnStart(numLevels, database);
         SceneManager.LoadScene(1);
+    }
+
+    private void setScale(GameObject container, Vector3 fill)
+    {
+        container.transform.position = new Vector3();
+    }
+
+    //offset and fill measured in percent of total screen safe area size
+    //TODO anything with scale?
+    private void setLocAndScale(GameObject container, Vector2 offset)
+    {
+        // Assuming you don't use this foolishly
+        float deltaX = safeArea.xMax - safeArea.xMin;
+        float deltaY = safeArea.yMax - safeArea.yMin;
+
+        container.transform.position = new Vector2(
+            safeArea.xMin + (deltaX * offset.x),
+            safeArea.yMin + (deltaY * offset.y)
+        );
+
+        //container.transform.localScale =
+        
     }
 
 
--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     Vector3 pos;
+    Vector2 touchPos;
+    float sumDistance = 0;
     public GameObject cam;
 
     // Start is called before the first frame update
@@ -16,6 +18,7 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // MOUSE CONTROLS
         if (Input.GetMouseButtonDown(2))
         {
             pos = Input.mousePosition;
@@ -40,6 +43,58 @@ public class PlayerManager : MonoBehaviour
         if (Input.mouseScrollDelta.y == -1)
         {
             cam.transform.position = cam.transform.position + new Vector3(0, 1, 0);
+        }
+
+
+        // TOUCH CONTROLS
+
+        if(Input.touchCount == 1)
+        {
+            Touch touch = Input.touches[0];
+            if(touch.phase == TouchPhase.Began)
+            {
+                touchPos = touch.position;
+            }
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Vector2 res = (touch.position - touchPos);
+                Vector3 transformation = new Vector3(-res.x, 0, -res.y) * 0.1f;
+                cam.transform.position = cam.transform.position + transformation;
+
+                pos = Input.mousePosition;
+            }
+        }
+
+        if(Input.touchCount == 2)
+        {
+            Touch first = Input.touches[0];
+            Touch second = Input.touches[1];
+
+            if(second.phase == TouchPhase.Began)
+            {
+                sumDistance = Vector2.Distance(first.position, second.position);
+            }
+
+            if(first.phase == TouchPhase.Moved || second.phase == TouchPhase.Moved)
+            {
+                float deltaDistance = sumDistance - Vector2.Distance(first.position, second.position);
+
+                // If they're getting closer, zoom out
+                if(deltaDistance < 0)
+                {
+                    cam.transform.position = cam.transform.position + new Vector3(0, 0.1f, 0);
+                }
+
+
+                // If they're getting further, zoom in
+                if(deltaDistance > 0)
+                {
+                    cam.transform.position = cam.transform.position + new Vector3(0, 0.1f, 0);
+                }
+
+                sumDistance = Vector2.Distance(first.position, second.position);
+            }
         }
     }
 }
